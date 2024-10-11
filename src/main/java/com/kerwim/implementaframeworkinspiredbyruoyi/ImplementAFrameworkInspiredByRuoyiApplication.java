23e40b53dc11b5ccde2c2351453d71b94c75a709
mac <0924>
2024-10-12 00:09:12 +0800

    intro

---
+++ src/main/java/com/kerwim/implementaframeworkinspiredbyruoyi/ImplementAFrameworkInspiredByRuoyiApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.implementaframeworkinspiredbyruoyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementAFrameworkInspiredByRuoyiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementAFrameworkInspiredByRuoyiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerwim/implementaframeworkinspiredbyruoyi/ImplementAFrameworkInspiredByRuoyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.implementaframeworkinspiredbyruoyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementAFrameworkInspiredByRuoyiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
